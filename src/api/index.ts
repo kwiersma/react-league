d6bde76f8bc9405268c8e7cd288d665aa799858b
Kurt Wiersma <kwiersma@gmail.com>
2019-07-27 16:15:36 -0500

    Added caching to the players and team data fetches

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,47 @@
 import {FantasyTeam, Player} from "../model";
 
 const baseUrl = "";
+let teams: FantasyTeam[];
+let players: Player[];
 
 const getFantasyTeams = (): Promise<FantasyTeam[]> => {
     const teamsUrl = baseUrl + "/api2/teams";
-    return fetch(teamsUrl, {credentials: "include"})
-        .then((response) => {
-            if (response.status === 404) {
-                return null;
-            }
-            return response.json();
-        })
-        .then((response: any): FantasyTeam[] => {
-            return response as FantasyTeam[];
-        });
+    if (teams) {
+        return new Promise<FantasyTeam[]>((resolve) => { return resolve(teams) });
+    } else {
+        return fetch(teamsUrl, {credentials: "include"})
+            .then((response) => {
+                if (response.status === 404) {
+                    return null;
+                }
+                return response.json();
+            })
+            .then((response: any): FantasyTeam[] => {
+                teams = response as FantasyTeam[];
+                console.log('teams fetched', teams);
+                return teams;
+            });
+    }
 };
 
 const getPlayers = (): Promise<Player[]> => {
     const playersUrl = baseUrl + "/api2/players";
-    return fetch(playersUrl, {credentials: "include"})
-        .then((response) => {
-            if (response.status === 404) {
-                return null;
-            }
-            return response.json();
-        })
-        .then((response: any): Player[] => {
-            return response as Player[];
-        });
+    if (players) {
+        return new Promise<Player[]>((resolve) => { return resolve(players) });
+    } else {
+        return fetch(playersUrl, {credentials: "include"})
+            .then((response) => {
+                if (response.status === 404) {
+                    return null;
+                }
+                return response.json();
+            })
+            .then((response: any): Player[] => {
+                players = response as Player[];
+                console.log('players fetched', players.length);
+                return players;
+            });
+    }
 };
 
 export const draftAPI = {
