a62ffba4f9fc9362acab74c5ea4b8e1ea9ab2cab
Kurt Wiersma <kwiersma@gmail.com>
2019-08-02 16:04:23 -0500

    Added a timer to the picks display

--- src/components/Picks.tsx
+++ src/components/Picks.tsx
@@ -7,9 +7,39 @@ interface PicksProps {
     picks: Pick[];
 }
 
-export class Picks extends Component<PicksProps, {}> {
+interface PicksState {
+    pickTimerStart: number;
+}
 
-    renderPicks(picks: Pick[]) {
+export class Picks extends Component<PicksProps, PicksState> {
+
+    constructor(props: PicksProps) {
+        super(props);
+
+        let startTime = new Date().getTime();
+        this.state = {
+            pickTimerStart: startTime
+        }
+    }
+
+    componentDidMount(): void {
+        let {pickTimerStart} = this.state;
+        if (this.props.picks.length > 2) {
+            pickTimerStart = new Date(this.props.picks[2].picktime).getTime();
+            this.setState({pickTimerStart})
+        }
+    }
+
+    renderPicks() {
+        const {picks} = this.props;
+
+        let pickTimerStart = 0;
+        if (picks.length > 2) {
+            pickTimerStart = new Date(this.props.picks[2].picktime).getTime();
+        } else {
+            pickTimerStart = new Date().getTime();
+        }
+
         return (
             <table id="picksTable">
                 <tbody>
@@ -26,8 +56,7 @@ export class Picks extends Component<PicksProps, {}> {
                     </td>
                     <td>{picks[2].player} by {picks[2].fantasyteam} ({picks[2].owner})</td>
                     <td rowSpan={2} align="center">
-                        <p style={{color: "black", fontSize: "20px", fontWeight: "bold"}}
-                           id="pickClock">0:00</p>
+                        <Timer startTime={pickTimerStart}/>
                         <span className="label label-danger">On the clock</span>
                     </td>
                 </tr>
@@ -53,7 +82,7 @@ export class Picks extends Component<PicksProps, {}> {
 
         let picksTable = <></>;
         if (picks.length > 0) {
-            picksTable = this.renderPicks(picks);
+            picksTable = this.renderPicks();
         }
 
         return (
@@ -62,5 +91,79 @@ export class Picks extends Component<PicksProps, {}> {
                 {picksTable}
             </div>
         );
+    }
+}
+
+interface TimerProps {
+    startTime: number
+}
+
+interface TimerState {
+    output: string;
+}
+
+class Timer extends React.Component<TimerProps, TimerState> {
+
+    interval?: any;
+
+    constructor(props: TimerProps) {
+        super(props);
+
+        this.state = {
+            output: "0:00"
+        }
+    }
+
+    componentDidMount(): void {
+        this.startPickTimer();
+    }
+
+    componentWillUnmount(): void {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+        this.interval = null;
+    }
+
+    componentDidUpdate(prevProps: Readonly<TimerProps>,
+                       prevState: Readonly<TimerState>,
+                       snapshot?: any): void {
+        if (prevProps.startTime != this.props.startTime) {
+            this.startPickTimer();
+        }
+    }
+
+    startPickTimer() {
+        var time = new Date().getTime() - this.props.startTime;
+        var elapsed = Math.floor(Math.abs(time) / 100) / 10;
+        elapsed = Math.round(elapsed);
+
+        var minutes = 0;
+        var seconds = elapsed;
+        if (elapsed >= 60) {
+            minutes = Math.floor(elapsed / 60);
+            seconds = elapsed - (minutes * 60);
+        }
+        var secondsStr: string = "";
+        if (seconds < 10) {
+            secondsStr = "0" + seconds.toString();
+        } else {
+            secondsStr = seconds.toString();
+        }
+        let output = minutes.toString() + ":" + secondsStr;
+        this.setState({output}, () => {
+            if (this.interval) {
+                clearInterval(this.interval);
+            }
+            this.interval = setInterval(() => {
+                this.startPickTimer();
+            }, 1000);
+        });
+    }
+
+    render() {
+        const {output} = this.state;
+        return <p style={{color: "black", fontSize: "20px", fontWeight: "bold"}}
+                  id="pickClock">{output}</p>;
     }
 }
