184bcdc53f1c26fc26401b74f99fa89ebb5e2940
Kurt Wiersma <kwiersma@gmail.com>
2020-05-01 16:46:41 -0500

    Added React-Testing-Library and a sample test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import App from "./App";
+import { render } from "@testing-library/react";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("should render app", async () => {
+  const { getByText } = render(<App />);
+
+  const navbar = await getByText("Ghetto League");
+  expect(navbar !== undefined);
 });
