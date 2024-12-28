827d0af4b6ed808064dcfbb514f10d0b9e1cff56
Kurt Wiersma <kwiersma@gmail.com>
2024-12-28 17:59:19 -0600

    Mocked remote data methods in the App.tsx test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 
+import { draftAPI } from './api';
 import App from './App';
 
+const returnEmptyArray = () => Promise.resolve([]);
+
 describe('should render app', () => {
+  const teamsMock = vi.spyOn(draftAPI, 'getFantasyTeams');
+  const playersMock = vi.spyOn(draftAPI, 'getPlayers');
+  const picksMock = vi.spyOn(draftAPI, 'getPicks');
+
+  teamsMock.mockImplementation(returnEmptyArray);
+  playersMock.mockImplementation(returnEmptyArray);
+  picksMock.mockImplementation(returnEmptyArray);
+
   it('should render headline', async () => {
     render(
       <BrowserRouter>
