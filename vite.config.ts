141631203031286a7ea960b479b7bd9f5a4e69fb
Kurt Wiersma <kwiersma@gmail.com>
2026-03-08 10:21:45 -0500

    Upgraded to eslint 9 and removed webvitals

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -24,7 +23,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), eslint()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
